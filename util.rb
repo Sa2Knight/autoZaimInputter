6c2715b2f389d0f217df98652a55407bd9b7d459
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-04-22 22:54:57 +0900

    Utilクラスのファイルを取得する処理を改良

--- util.rb
+++ util.rb
@@ -2,12 +2,23 @@ require 'json'
 class Util
 
   KEYSFILE = "keys.json"
+  TEMPLATEFILE = "template.json"
+
+  # JSONファイルを取得
+  def self.get_json(path)
+    File.open(path) do |f|
+      JSON.load(f)
+    end
+  end
 
   # APIキーを取得
   def self.get_api_key
-    File.open(KEYSFILE) do |f|
-      keys = JSON.load(f)
-    end
+    self.get_json(KEYSFILE)
+  end
+
+  # 登録テンプレートを取得
+  def self.get_template
+    self.get_json(TEMPLATEFILE)
   end
 
 end
